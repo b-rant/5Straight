2a45f2860e8690188c03f92c6efc15d9fc1524aa
blakedh <63508850+blakedh@users.noreply.github.com>
2020-04-18 10:33:23 -0500

    Adds client callbacks to Game object and client registration method to GameManager

--- 5Straight/Data/GameManager.cs
+++ 5Straight/Data/GameManager.cs
@@ -1,8 +1,9 @@
 ﻿using _5Straight.Data.Models;
 using _5Straight.Data.Proxies;
+using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
 
 namespace _5Straight.Data
 {
@@ -10,12 +11,17 @@ namespace _5Straight.Data
     {
         public Dictionary<string, Game> Games;
         public GameStateTable GameTable;
+        public delegate Task ClientCallback();
 
         public GameManager(GameStateTable gameStateTable)
         {
             Games = new Dictionary<string, Game>();
 
             GameTable = gameStateTable;
+        }
+        public void ConnectClientToGame(string id, ClientCallback callback)
+        {
+            Games[id].Clients.Add(callback);
         }
 
         public Game CreateNewGame(string gameName, int numTeams, int numPlayersPerTeam)
@@ -25,6 +31,18 @@ namespace _5Straight.Data
             Games.Add(game.PartitionKey, game);
 
             return game;
+        }
+
+        public List<Game> GetOpenGames()
+        {
+            return new List<Game>();
+            // comes from Table Service
+        }
+
+        public List<Game> GetActiveGamesForUser(string uName)
+        {
+            return new List<Game>();
+            // comes from Table Service
         }
     }
 }
